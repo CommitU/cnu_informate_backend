09a56649cb435586e6d702b2d2cd0c9ded135c29
LYJ0304 <omlmg0304@naver.com>
2025-08-07 15:38:15 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/commitU/informate/InformateApplication.java
@@ -0,0 +1,13 @@
+package com.commitU.informate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commitU/informate/InformateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commitU.informate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
